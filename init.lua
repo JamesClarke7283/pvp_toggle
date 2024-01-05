af148675a6c030a905c46054d5306b2f60e15bb1
James David Clarke <james@jamesdavidclarke.com>
2024-01-05 13:14:50 +0000

    Add logging

--- init.lua
+++ init.lua
@@ -68,6 +68,8 @@ if minetest.get_modpath("mcl_inventory") then
                 if not is_pvp_enabled(obj) or not is_pvp_enabled(reason.source) then
                     if minetest.get_modpath("mcl_burning") then
                         mcl_burning.extinguish(obj)
+                        minetest.log("action", "[PvP Mod] Extinguishing player " .. obj:get_player_name())
+                        minetest.log("action", "[PvP Mod] Extinguishing source player " .. reason.source:get_player_name())
                         mcl_burning.extinguish(reason.source)  -- Extinguish the player if they are on fire
                     end
                     return 0 -- No damage if PvP is disabled for either player
